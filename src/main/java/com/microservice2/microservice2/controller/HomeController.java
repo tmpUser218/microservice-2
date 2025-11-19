23a89154240c120fd5716d51bbaec40a28cc2c81
mofhassan <mo.hassan272002@gmail.com>
2025-11-19 10:59:33 +0200

    Add prod branch.

---
+++ src/main/java/com/microservice2/microservice2/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.microservice2.microservice2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World from Microservice 2! - PROD";
+    }
+}
