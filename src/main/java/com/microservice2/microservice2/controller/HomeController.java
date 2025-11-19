f5e2bbb24ca12470be5cf938f65b90ab7f32a40b
mofhassan <mo.hassan272002@gmail.com>
2025-11-19 11:00:58 +0200

    Add dev branch.

--- src/main/java/com/microservice2/microservice2/controller/HomeController.java
+++ src/main/java/com/microservice2/microservice2/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World from Microservice 2! - UAT";
+        return "Hello World from Microservice 2! - DEV";
     }
 }
